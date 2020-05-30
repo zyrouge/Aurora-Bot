f3bc635d117de44c99963894a2159084a00666a3
zyrouge <naveencnaveen96@gmail.com>
2020-05-30 10:28:53 +0530

    Fixed License in package.json

--- config.js
+++ config.js
@@ -3,12 +3,11 @@
  * @license GPL-3.0
 */
 
-const state = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
-
 const config = {
-    state,
+    state: process.env.NODE_ENV ? process.env.NODE_ENV : "development",
+    prod: process.env.NODE_ENV === "production",
     token: process.env.DISCORDTOKEN,
-    prefix: `a&`,
+    prefix: this.prod ? "a&" : "a$",
     invite: `https://discord.com/oauth2/authorize?client_id=702808552892530829&permissions=2146827775&response_type=code&scope=bot`,
     support: "https://discord.com/invite/8KV5zCg",
     dashboard: "https://auroradiscordbot.ga",
