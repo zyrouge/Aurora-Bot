a63d69e1a7658f27f30209b28d70c44908e20f22
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-07 12:10:17 +0545

    feat: default avatar

--- src/Utils/parseAvatar.js
+++ src/Utils/parseAvatar.js
@@ -6,5 +6,5 @@
 module.exports = async (user) => {
     return user.avatar ? 
         `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${user.avatar.includes("a_") ? "gif": "png"}` :
-        `https://cdn.discordapp.com/embed/avatars/${user.discriminator % 4}.png`
+        `https://cdn.discordapp.com/embed/avatars/${user.discriminator % 5}.png`
 }
