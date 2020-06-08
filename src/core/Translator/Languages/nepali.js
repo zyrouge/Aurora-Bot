7d8ee85856aa380ef6e06ce9494879e2676a29d0
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-08 22:42:38 +0545

    Update nepali.js

--- src/core/Translator/Languages/nepali.js
+++ src/core/Translator/Languages/nepali.js
@@ -4,14 +4,15 @@
 */
 
 module.exports = {
+    COMMAND_DISABLED:       (COMMAND) => `आदेश \`${COMMAND}\`लाई यो संजालमा अक्षम गरिएको छ।`,
     PREFIX_CHANGE:          (PREFIX) => `प्रीफिक्स ${PREFIX} मा परिवर्तन भएको छ।`,
     HELP_DESC:              (PREFIX, COMMANDS) => (
-                                `कोटिमा रहेका आदेशको प्रयोग गर्न \`${PREFIX}help <संख्या>\` को प्रयोग गर्नुहोला\n`+
-                                `उदाहरणका लागि, **Utility** कोटिमा रहेका आदेशका लागि \`${PREFIX}help 13\` प्रयोग गर्नुहोला।\n`+
+                                `कोटीमा रहेका आदेशको प्रयोग गर्न \`${PREFIX}help <संख्या>\` को प्रयोग गर्नुहोला\n`+
+                                `उदाहरणका लागि, **Utility** कोटीमा रहेका आदेशका लागि \`${PREFIX}help 13\` प्रयोग गर्नुहोला।\n`+
                                 `जम्मा आदेशहरु: **${COMMANDS}**`
                             ),
     HELP_MENU:              "सहयोगी मेनू",
-    HELP_CATEGORIES:        "कोटिहरू",
+    HELP_CATEGORIES:        "कोटीहरू",
     HELP_SUPPORT:           "सहयोगका लागी",
     DASHBOARD:              "प्रबन्ध कक्ष",
     WEBSITE:                "वेबसाईट",
