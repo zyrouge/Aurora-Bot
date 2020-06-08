29245030aa0bafdf196ed2d3ed3eb7c7653c241a
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-08 22:57:01 +0545

    Update nepali.js

--- src/core/Translator/Languages/nepali.js
+++ src/core/Translator/Languages/nepali.js
@@ -4,19 +4,20 @@
 */
 
 module.exports = {
+    COMMAND_DISABLED:       (COMMAND) => `आदेश \`${COMMAND}\`लाई यो संजालमा अक्षम गरिएको छ।`,
     PREFIX_CHANGE:          (PREFIX) => `प्रीफिक्स ${PREFIX} मा परिवर्तन भएको छ।`,
-    HELP_DESC:              (PREFIX, COMMANDS) => (
-                                `कोटिमा रहेका आदेशको प्रयोग गर्न \`${PREFIX}help <संख्या>\` को प्रयोग गर्नुहोला\n`+
-                                `उदाहरणका लागि, **Utility** कोटिमा रहेका आदेशका लागि \`${PREFIX}help 13\` प्रयोग गर्नुहोला।\n`+
+    HELP_DESC:              (PREFIX, COMMANDS, RANDOMCATNUMBER, RANDOMCATNAME) => (
+                                `कोटीमा रहेका आदेशको प्रयोग गर्न \`${PREFIX}help <संख्या>\` को प्रयोग गर्नुहोला\n`+
+                                `उदाहरणका लागि, **${RANDOMCATNAME.toProperCase()}** कोटीमा रहेका आदेशका लागी \`${PREFIX}help ${RANDOMCATNUMBER}\` प्रयोग गर्नुहोला।\n`+
                                 `जम्मा आदेशहरु: **${COMMANDS}**`
                             ),
     HELP_MENU:              "सहयोगी मेनू",
-    HELP_CATEGORIES:        "कोटिहरू",
+    HELP_CATEGORIES:        "कोटीहरू",
     HELP_SUPPORT:           "सहयोगका लागी",
     DASHBOARD:              "प्रबन्ध कक्ष",
     WEBSITE:                "वेबसाईट",
     TRANSLATION_ERROR:      "भाषा परिवर्तनमा केही समस्या आएको छ।",
-    GUILD_LANGUAGE:         "यो संजालको भषा नेपाली हो।",
+    GUILD_LANGUAGE:         "यो संजालको भाषा नेपाली हो।",
     "1":                    "१",
     "2":                    "२",
     "3":                    "३",
